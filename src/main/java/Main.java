10dfa87088688f29b2c8f29480a252b914ba11e5
amirhkk <amir.hkkh@gmail.com>
2022-05-10 15:17:39 +0100

    Simple Frame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args){
         JFrame frame = new JFrame();
-        frame.setSize(100,100);
+        frame.setSize(450,694);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("WeatherApp");
         frame.setVisible(true);
