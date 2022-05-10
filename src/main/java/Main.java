afb29616ce63eb2e12ffc01d3a11a711c6016831
amirhkk <amir.hkkh@gmail.com>
2022-05-10 15:14:09 +0100

    Simple Frame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import javax.swing.JFrame;
 public class Main {
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setSize(100,100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("WeatherApp");
+        frame.setVisible(true);
+    }
 }
