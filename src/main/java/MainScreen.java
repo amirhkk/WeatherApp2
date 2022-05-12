26365d429df70445202fa376391cc00c99ea9dc6
Justin Lam <jl2261@cam.ac.uk>
2022-05-12 14:33:14 +0100

    GUI test

--- src/main/java/MainScreen.java
+++ src/main/java/MainScreen.java
@@ -1,11 +1,25 @@
 import javax.swing.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainScreen extends JFrame {
     private JPanel panel1;
     private JButton settingsButton;
     private JSlider slider1;
+    private JLabel warningLabel;
+    private JLabel actualTempLabel;
+    private JLabel feltTempLabel;
+    private JLabel weatherIconLabel;
+    private JLabel dateLabel;
+    private ImageIcon weatherIcon;
 
     public MainScreen() {
         setSize(450, 700);
+        try {
+            weatherIcon = new ImageIcon(new URL("https://cdn-icons-png.flaticon.com/512/4052/4052984.png"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        weatherIconLabel.setIcon(weatherIcon);
     }
 }
