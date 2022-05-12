83de3c2b335cf2fca275a170ebf28c59fe884ddd
Justin Lam <jl2261@cam.ac.uk>
2022-05-12 14:26:46 +0100

    GUI test

--- src/main/java/MainScreen.java
+++ src/main/java/MainScreen.java
@@ -4,4 +4,8 @@ public class MainScreen {
     private JPanel panel1;
     private JButton settingsButton;
     private JSlider slider1;
+
+    public MainScreen() {
+        panel1.setSize(450, 700);
+    }
 }
