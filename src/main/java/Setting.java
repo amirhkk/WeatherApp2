c15caa92629cab8598c0bf0d987d50988314e0e6
Tamisa Ketmalasiri <tketmalasiri@gmail.com>
2022-05-12 15:35:43 +0100

    update setting

--- src/main/java/Setting.java
+++ src/main/java/Setting.java
@@ -1,5 +1,23 @@
 import javax.swing.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class Setting {
-    private JRadioButton radioButton1;
+public class Setting extends JFrame{
+    private JLabel notiLabel;
+    private JLabel settingLabel;
+    private JButton backButton;
+    private JLabel tempLabel;
+    private JLabel precLabel;
+    private JLabel rainLabel;
+    private JLabel stormLabel;
+    private JCheckBox tempCheckBoxCheckBox;
+    private JCheckBox precCheckBoxCheckBox;
+    private JCheckBox rainCheckBoxCheckBox;
+    private JCheckBox stormCheckBoxCheckBox;
+    private JPanel panel;
+
+    public Setting() {
+        setSize(450, 700);
+        add(panel);
+    }
 }
