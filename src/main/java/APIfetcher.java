39b872c4c4df0119ea14ee9d93f0de848c0f6558
Leon Elias Kollar <lek49@cam.ac.uk>
2022-05-12 14:03:31 +0100

    Added query for hourly forecast for the next 2 days

--- src/main/java/APIfetcher.java
+++ src/main/java/APIfetcher.java
@@ -1,3 +1,4 @@
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -5,9 +6,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class APIfetcher {
@@ -47,6 +48,28 @@ public class APIfetcher {
     public static String getCurrentFeltTemp(){
         APIfetcher.update();
         return Double.toString(Double.parseDouble(currentData.json.getAsJsonObject("current").get("feels_like").getAsString()) - 273.15);
+    }
+
+    public static Map<String, String> getHourlyAt(String hour){
+        APIfetcher.update();
+        int hourAsInt = Integer.parseInt(hour);
+        if (hourAsInt > 47 || hourAsInt < 0) {
+            throw new IllegalArgumentException("We only provide forecasts for the next 48hrs (0-47");
+        }
+
+        Map<String, String> result = new HashMap<>();
+        //System.out.println(currentData.json.get("hourly").getAsJsonObject().getAsJsonObject(hour));
+
+        // JsonObject queryResult = currentData.json.getAsJsonArray().get(hour);
+        JsonObject queryResult = currentData.json.getAsJsonArray("hourly").get(hourAsInt).getAsJsonObject(); //.getAsJsonObject(hour);
+        //JsonObject queryResult = currentData.json.getAsJsonObject("hourly"); //.getAsJsonObject(hour);
+        //System.out.println(queryResult.get("temp"));
+        result.put("actual", Double.toString(Double.parseDouble(queryResult.get("temp").getAsString()) -273.15));
+        result.put("felt", Double.toString(Double.parseDouble(queryResult.get("feels_like").getAsString()) - 273.15));
+
+        return result;
+
+
     }
 
 }
--- src/main/java/mainAPI.java
+++ src/main/java/mainAPI.java
@@ -1,8 +1,11 @@
+import java.util.Map;
+
 public class mainAPI{
     public static void main(String[] args) {
-        //APIfetcher.update();
         String tmp = APIfetcher.getCurrentActualTemp();
         String felt = APIfetcher.getCurrentFeltTemp();
         System.out.println("current temp: " + tmp + ". current felt temp: " + felt);
+        Map<String, String> hourly = APIfetcher.getHourlyAt("47");
+        System.out.println(hourly.get("actual") + "\n" + hourly.get("felt"));
     }
 }
