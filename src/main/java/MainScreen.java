9e517b2fde97e3117dbb2ec9b6554f45406f4a63
Justin Lam <jl2261@cam.ac.uk>
2022-05-15 17:33:25 +0100

    mainscreen

--- src/main/java/MainScreen.java
+++ src/main/java/MainScreen.java
@@ -14,18 +14,16 @@ public class MainScreen extends JFrame {
     private JLabel weatherIconLabel;
     private JLabel dateLabel;
     private JButton warningButton;
-    private ImageIcon weatherIcon;
-    private ImageIcon warningIcon;
 
     public MainScreen() {
         setSize(450, 700);
 
-        weatherIcon = new ImageIcon("src/main/java/Icons/02d.png");
-        weatherIcon = new ImageIcon(weatherIcon.getImage().getScaledInstance(150, 150,  java.awt.Image.SCALE_SMOOTH));
+        ImageIcon weatherIcon = new ImageIcon(new ImageIcon("src/main/java/Icons/02d.png")
+                .getImage().getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH));
         weatherIconLabel.setIcon(weatherIcon);
 
-        warningIcon = new ImageIcon("src/main/java/Icons/Warning.png");
-        warningIcon = new ImageIcon(warningIcon.getImage().getScaledInstance(40, 40,  java.awt.Image.SCALE_SMOOTH));
+        ImageIcon warningIcon = new ImageIcon(new ImageIcon("src/main/java/Icons/Warning.png")
+                .getImage().getScaledInstance(40, 40,  java.awt.Image.SCALE_SMOOTH));
         warningButton.setIcon(warningIcon);
 
         String actualTemp = APIfetcher.getCurrentActualTemp();
