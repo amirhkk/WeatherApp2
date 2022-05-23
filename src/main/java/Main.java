6bfe288d4b3324d3b2613ca8e09087c3739aa373
Peter Isaksen <pi241@cam.ac.uk>
2022-05-23 17:09:42 +0100

    Removed check for Windows in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,10 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        if (System.getProperty("os.name").contains("Windows")) {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         RootScreen rootScreen = new RootScreen();
         rootScreen.setVisible(true);
